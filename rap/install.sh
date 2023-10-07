d3c06f349b0d30594dc6d123c67f185560331ef8
VaynNecol <vayn.necol@outlook.com>
2023-10-07 13:10:19 +0800

    refine `echo` messages to be easy to read

--- rap/install.sh
+++ rap/install.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-echo "Building RAP and install RAP by Cargo"
+echo -e "\e[1;31mBuilding and install \e[1;36mrap \e[1;31minto \e[1;36mcargo\e[1;31m.\e[0m"
 
 # Execution self cleanup procedure
 cargo clean
@@ -23,8 +23,10 @@ export DYLD_LIBRARY_PATH=/<working-dir>/RAP/rust/build/<host-triple>/stage2/lib/
 export RUSTFLAGS="-L /<working-dir>/RAP/rust/build/<host-triple>/stage2/lib":$RUSTFLAGS
 export RUSTFLAGS="-L /<working-dir>/RAP/rust/build/<host-triple>/stage2/lib/rustlib/<host-triple>/lib":$RUSTFLAGS
 
+echo -e "\e[1;32mBuilding and installing \e[1;36mrap \e[1;32mfinished.\e[0m"
+
 # Build and install `rap-llvm` into user local bins
-echo "Building RAP_Phase_LLVM by CMake and add the tool into Environment"
+echo -e "\e[1;32mBuilding \e[1;36mrap-llvm \e[1;32mby \e[1;36mCMake \e[1;32mand add to local bins.\e[0m"
 
 # Generate building directory
 cd phase_llvm || exit
@@ -35,6 +37,7 @@ cd cmake-build || exit
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_DEPENDS_USE_COMPILER=FALSE -G "CodeBlocks - Unix Makefiles" "../../$(dirname "$0")"
 cmake --build "$(dirname "$0")" --target rap_phase_llvm -v -- -j 9
 
+echo -e "\e[1;32mBuilding and installing \e[1;36mrap-llvm \e[1;32mfinished.\e[0m"
 
 # Write environment variables into usr system
 p="export PATH=\"\$PATH:${PWD}/\""
@@ -45,3 +48,7 @@ echo $p >> ~/.zshrc
 echo $p >> ~/.bashrc
 
 export PATH="$PATH:${PWD}"
+
+echo -e "\e[1;33mBuild and install all components successfully.\e[0m"
+echo -e "\e[1;33mPress any key to exit...\e[0m"
+read -n 1
