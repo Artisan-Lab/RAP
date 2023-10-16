40cc00fafa6bd31a9f5264b2c8deeaa88f6fbc52
VaynNecol <vayn.necol@outlook.com>
2023-10-16 15:35:57 +0800

    Update 00-install-rap-rust.sh

--- 00-install-rap-rust.sh
+++ 00-install-rap-rust.sh
@@ -1,50 +1,54 @@
-#!/bin/zsh
+RED='\033[4;31m'
+YELLOW='\033[4;33m'
+GREEN='\033[0;32m'
+BLUE='\033[36m'
+WHITE='\033[4m'
+NC='\033[0m'
 
+printf "%bNow building cargo rap for your toolchain.%b\n" "${YELLOW}" "${NC}"
+printf "%bPHASE1: Checking operating system.%b\n" "${YELLOW}" "${NC}"
 os_type=$(uname -s)
-
-echo "\e[4;33mNow building cargo rap for your toolchain.\e[0m"
-echo "\e[4;33mPHASE1: Checking operating system.\e[0m"
 if [ "$os_type" = "Linux" ]; then
-    echo "Detection success: running on \e[1;36mLinux (x86_64-unknown-linux-gnu)\e[0m."
+    printf "Detection success: running on %bLinux (x86_64-unknown-linux-gnu)%b.\n" "${BLUE}" "${NC}"
     export HOST_TRIPLE="x86_64-unknown-linux-gnu"
 elif [ "$os_type" = "Darwin" ]; then
-    echo "Detection success: running on \e[1;36mMacintosh (x86_64-apple-darwin)\e[0m."
+    printf "Detection success: running on %bMacintosh (x86_64-apple-darwin)%b.\n" "${BLUE}" "${NC}"
     export HOST_TRIPLE="x86_64-apple-darwin"
 elif [ "$os_type" = "FreeBSD" ]; then
-    echo "Detection success: running on \e[1;36mFreeBSD (x86_64-unknown-linux-gnu)\e[0m."
+    printf "Detection success: running on %bFreeBSD (x86_64-unknown-linux-gnu)%b.\n" "${BLUE}" "${NC}"
     export HOST_TRIPLE="x86_64-unknown-linux-gnu"
 else
-    echo "Detection failed: running unsupported operating system: $os_type."
-    echo "\e[4mPress any key to exit...\e[0m"
+    printf "Detection failed: running unsupported operating system: $os_type.\n"
+    printf "%bPress any key to exit...%b\n" "${WHITE}" "${NC}"
     read -n 1
     exit 1
 fi
 
-echo "\e[4;33mPHASE2: Checking build dependencies \e[1;36mrustup\e[4;33m.\e[0m"
+printf "%bPHASE2: Checking build dependencies %brustup%b.%b\n" "${YELLOW}" "${BLUE}" "${YELLOW}" "${NC}"
 export RUSTUP_SHOW=$(rustup show)
 export RAP_DIR=$(dirname "$(readlink -f "$0")")
 if [ -z "$(ls ${RAP_DIR}/rust)" ]; then
-    echo "Detection failed: directory of rap-rust is empty, please update submodule first."
-    echo "\e[4mPress any key to exit...\e[0m"
+    printf "Detection failed: directory of rap-rust is empty, please update submodule first.\n"
+    printf "%bPress any key to exit...%b\n" "${WHITE}" "${NC}"
     read -n 1
     exit 2
 else
   if echo "${RUSTUP_SHOW}" | grep -q "rustup"; then
-    echo "Detection success: \e[1;36mrustup\e[0m has been linked into \e[1;36mrustup\e[0m toolchain."
+    printf "Detection success: %brustup%b has been linked into %brustup%b toolchain.\n" "${BLUE}" "${NC}" "${BLUE}" "${NC}"
   else
-    echo "Detection failed: cannot find \e[1;36mrustup\e[0m, please install \e[1;36mrustup\e[0m first."
-    echo "\e[4mPress any key to exit...\e[0m"
+    printf "Detection failed: cannot find %brustup%b, please install %brustup%b first.\n" "${BLUE}" "${NC}" "${BLUE}" "${NC}"
+    printf "%bPress any key to exit...%b\n" "${WHITE}" "${NC}"
     read -n 1
     exit 2
   fi
 fi
 
-echo "\e[4;31mPHASE3: Building, installing and linking \e[1;36mrap-rust \e[4;31minto \e[1;36mcargo\e[4;31m.\e[0m"
+printf "%bPHASE3: Building, installing and linking %brap-rust %binto %bcargo%b.%b\n" "${RED}" "${BLUE}" "${RED}" "${BLUE}" "${RED}" "${NC}"
 cp -f ./config.toml ./rust/config.toml
 cd rust && ./x.py build compiler/rustc -i --stage 2
 rustup toolchain link rap-rust build/${HOST_TRIPLE}/stage2
 
-echo "\e[0;32mBuilding success: building, installing and linking \e[1;36mrap-rust \e[0;32mfinished.\e[0m"
-echo "\e[1;33mBuild and install all components successfully.\e[0m"
-echo "\e[4mPress any key to exit...\e[0m"
+printf "%bBuilding success: building, installing and linking %brap-rust %bfinished.%b\n" "${GREEN}" "${BLUE}" "${GREEN}" "${NC}"
+printf "%bBuild and install all components successfully.%b\n" "${YELLOW}" "${NC}"
+printf "%bPress any key to exit...%b\n" "${WHITE}" "${NC}"
 read -n 1
