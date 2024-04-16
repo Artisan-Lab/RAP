27e983d81cc0498ad46c81f8331a727238504908
aisr <china.xuhui@gmail.com>
2024-04-16 13:52:59 +0800

    refector test cases

--- test cases/uaf_small/target/x86_64-unknown-linux-gnu/debug/deps/dangling_min-6294142d6d81d943.d
+++
@@ -1,5 +0,0 @@
-/home/aisr/RAP/test cases/uaf_min/target/x86_64-unknown-linux-gnu/debug/deps/libdangling_min-6294142d6d81d943.rmeta: src/main.rs
-
-/home/aisr/RAP/test cases/uaf_min/target/x86_64-unknown-linux-gnu/debug/deps/dangling_min-6294142d6d81d943.d: src/main.rs
-
-src/main.rs:
