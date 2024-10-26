a2de468ce219295856a331758fa7800c3eeba3a9
zjp <jiping_zhou@foxmail.com>
2024-10-26 15:38:38 +0800

    chore: remove get_arg_flag_value

--- rap/src/bin/cargo-rap/args.rs
+++ rap/src/bin/cargo-rap/args.rs
@@ -15,29 +15,6 @@ struct Arguments {
 }
 
 impl Arguments {
-    // Get value from `name=val` or `name val`.
-    fn get_arg_flag_value(&self, name: &str) -> Option<&str> {
-        let mut args = self.args_group1.iter();
-
-        while let Some(arg) = args.next() {
-            if !arg.starts_with(name) {
-                continue;
-            }
-            // Strip leading `name`.
-            let suffix = &arg[name.len()..];
-            if suffix.is_empty() {
-                // This argument is exactly `name`; the next one is the value.
-                return args.next().map(|x| x.as_str());
-            } else if suffix.starts_with('=') {
-                // This argument is `name=value`; get the value.
-                // Strip leading `=`.
-                return Some(&suffix[1..]);
-            }
-        }
-
-        None
-    }
-
     fn new() -> Self {
         let args: Vec<_> = env::args().collect();
         let path = env::current_exe().expect("Current executable path invalid.");
@@ -73,10 +50,6 @@ fn split_args_by_double_dash(args: &[String]) -> [Vec<String>; 2] {
 
 static ARGS: LazyLock<Arguments> = LazyLock::new(Arguments::new);
 
-pub fn get_arg_flag_value(name: &str) -> Option<&'static str> {
-    ARGS.get_arg_flag_value(name)
-}
-
 /// `cargo rap [rap options] -- [cargo check options]`
 ///
 /// Options before the first `--` are arguments forwarding to rap.
@@ -88,12 +61,6 @@ pub fn rap_and_cargo_args() -> [&'static [String]; 2] {
 /// If a crate being compiled is local in rustc phase.
 pub fn is_current_compile_crate() -> bool {
     ARGS.is_current_compile_crate()
-}
-
-pub fn is_crate_type_lib() -> bool {
-    get_arg_flag_value("--crate--type")
-        .map(crate::TargetKind::is_lib_str)
-        .unwrap_or(false)
 }
 
 pub fn get_arg(pos: usize) -> Option<&'static str> {
