8bfefb1a25bb5c8cfefba169c83470d9e7cf150a
zjp <jiping_zhou@foxmail.com>
2024-11-14 09:22:19 +0800

    chore: move aroud code blocks

--- rap/src/bin/cargo-rap/cargo_check.rs
+++ rap/src/bin/cargo-rap/cargo_check.rs
@@ -7,7 +7,7 @@ use rap::utils::log::rap_error_and_exit;
 use std::{collections::BTreeMap, process::Command, time::Duration};
 use wait_timeout::ChildExt;
 
-pub fn run(dir: &Utf8Path) {
+fn run(dir: &Utf8Path) {
     let [rap_args, cargo_args] = args::rap_and_cargo_args();
     rap_debug!("rap_args={rap_args:?}\tcargo_args={cargo_args:?}");
 
@@ -52,36 +52,20 @@ pub fn run(dir: &Utf8Path) {
     };
 }
 
-pub fn get_cargo_tomls_deep_recursively(dir: &str) -> Vec<Utf8PathBuf> {
-    walkdir::WalkDir::new(dir)
-        .into_iter()
-        .filter_map(|entry| {
-            if let Ok(e) = entry {
-                if e.file_type().is_file() && e.file_name().to_str()? == "Cargo.toml" {
-                    let path = Utf8PathBuf::from_path_buf(e.into_path());
-                    return path.ok()?.canonicalize_utf8().ok();
-                }
-            }
-            None
-        })
-        .collect()
+/// Just like running a cargo check in a folder.
+pub fn default_run() {
+    run(".".into());
+}
+
+/// Run cargo check in each member folder under current workspace.
+pub fn shallow_run() {
+    let ws_metadata = workspace(".".into());
+    for pkg_folder in get_member_folders(&ws_metadata) {
+        run(pkg_folder);
+    }
 }
 
 type Workspaces = BTreeMap<Utf8PathBuf, Metadata>;
-
-fn workspaces(cargo_tomls: &[Utf8PathBuf]) -> Workspaces {
-    let mut map = BTreeMap::new();
-    for cargo_toml in cargo_tomls {
-        let metadata = workspace(cargo_toml);
-        let root = &metadata.workspace_root;
-        // 每个 member package 解析的 workspace_root 和 members 是一样的
-        if !map.contains_key(root) {
-            map.insert(root.clone(), metadata);
-        }
-    }
-
-    map
-}
 
 fn workspace(cargo_toml: &Utf8Path) -> Metadata {
     let exec = cargo_metadata::MetadataCommand::new()
@@ -107,19 +91,6 @@ fn get_member_folders(meta: &Metadata) -> Vec<&Utf8Path> {
         .collect()
 }
 
-/// Just like running a cargo check in a folder.
-pub fn default_run() {
-    run(".".into());
-}
-
-/// Run cargo check in each member folder under current workspace.
-pub fn shallow_run() {
-    let metadata = workspace(".".into());
-    for pkg_folder in get_member_folders(&metadata) {
-        run(pkg_folder);
-    }
-}
-
 /// Recursively run cargo check in each package folder from current folder.
 pub fn deep_run() {
     let cargo_tomls = get_cargo_tomls_deep_recursively(".");
@@ -128,4 +99,33 @@ pub fn deep_run() {
             run(pkg_folder);
         }
     }
+}
+
+fn get_cargo_tomls_deep_recursively(dir: &str) -> Vec<Utf8PathBuf> {
+    walkdir::WalkDir::new(dir)
+        .into_iter()
+        .filter_map(|entry| {
+            if let Ok(e) = entry {
+                if e.file_type().is_file() && e.file_name().to_str()? == "Cargo.toml" {
+                    let path = Utf8PathBuf::from_path_buf(e.into_path());
+                    return path.ok()?.canonicalize_utf8().ok();
+                }
+            }
+            None
+        })
+        .collect()
+}
+
+fn workspaces(cargo_tomls: &[Utf8PathBuf]) -> Workspaces {
+    let mut map = BTreeMap::new();
+    for cargo_toml in cargo_tomls {
+        let metadata = workspace(cargo_toml);
+        let root = &metadata.workspace_root;
+        // 每个 member package 解析的 workspace_root 和 members 是一样的
+        if !map.contains_key(root) {
+            map.insert(root.clone(), metadata);
+        }
+    }
+
+    map
 }
