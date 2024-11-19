f8f73f144f357be11c370d02104db8acb78c619f
Calvin <CalvinCui@foxmail.com>
2024-11-19 16:43:23 +0800

    add test case for bounds loop push

--- rap/src/analysis/opt/checking/bounds_checking.rs
+++ rap/src/analysis/opt/checking/bounds_checking.rs
@@ -6,6 +6,6 @@ pub mod bounds_len;
 pub mod bounds_loop_push;
 
 pub fn check(graph: &Graph, tcx: &TyCtxt) {
-    // bounds_len::check(graph, tcx);
+    bounds_len::check(graph, tcx);
     bounds_loop_push::check(graph, tcx);
 }
