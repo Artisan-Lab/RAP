2657413c87057667c8f776b3908697c9aebd6ae2
VaynNecol <vayn.necol@outlook.com>
2024-12-17 22:03:39 +0800

    make defid correct for new rap version

--- rap/src/analysis/utils/intrinsic_id.rs
+++ rap/src/analysis/utils/intrinsic_id.rs
@@ -1,9 +1,9 @@
 /*
  * NOTE: This should be updated with different rustc versions.
  */
-pub const DROP: usize = 2105;
-pub const DROP_IN_PLACE: usize = 2652;
-pub const MANUALLYDROP: usize = 1948;
+pub const DROP: usize = 2177;
+pub const DROP_IN_PLACE: usize = 2744;
+pub const MANUALLYDROP: usize = 2018;
 pub const CALL_MUT: usize = 3575;
 pub const NEXT: usize = 8583;
 pub const CLONE: usize = 2913;
