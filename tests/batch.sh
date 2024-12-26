1a24dc0823fc8e7102b4f2fd1dfead160a91d8f1
zjp <jiping_zhou@foxmail.com>
2024-12-26 13:31:57 +0800

    test: save output to rap.txt and display it if error

--- tests/batch.sh
+++ tests/batch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #该脚本在目录下为每个Cargo项目执行相同的命令直到报错
 
+cur=$(pwd)
+
 # 查找并编译当前目录下的所有 Rust 项目
 find support -type f -name "Cargo.toml" | while read -r cargo_file; do
   # 获取 Cargo.toml 文件所在的目录
@@ -20,12 +22,13 @@ find support -type f -name "Cargo.toml" | while read -r cargo_file; do
     #脚本有参数时按照给定参数执行cargo命令
     #Example: batch.sh rap -uaf
     cmd="cargo rap $@"
-    $cmd 2>&1 | grep 'RAP|WARN|'
+    $cmd 2>&1 | tee $cur/rap.txt | grep 'RAP|WARN|'
   fi
 
   if [ $? -ne 0 ]; then
     # 如果命令失败，打印错误信息并退出循环
-    echo "Error: '$cmd' failed in $project_dir"
+    echo -e "Error: '$cmd' failed in $project_dir \nRAP output:"
+    cat $cur/rap.txt
     popd >/dev/null
     exit 1
   fi
