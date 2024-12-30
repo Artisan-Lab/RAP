ddeb8017e1329454d478a6a2cabe0bc36447b180
aisr <china.xuhui@gmail.com>
2024-12-31 00:26:40 +0800

    update tests/uaf

--- tests/support/uaf/uaf_drop_in_palce/src/main.rs
+++ tests/support/uaf/uaf_drop_in_palce/src/main.rs
@@ -1,6 +1,5 @@
 #![feature(fn_traits)]
 use std::ptr::drop_in_place;
-// ptr::drop_in_place
 fn main() {
     let mut x = Box::from(0);
     let ptr = &mut x as *mut Box<i32>;
@@ -8,11 +7,4 @@ fn main() {
     unsafe {
         drop_in_place(ptr);
     }
-    let numbers = vec![10, 20, 30, 40, 50];
-    let mut iter = numbers.iter();
-    println!("{:?}", iter.next()); 
-
-    let mut call_count = 0;
-    let mut c = || { call_count += 1; };
-    c.call_mut(());
 }
